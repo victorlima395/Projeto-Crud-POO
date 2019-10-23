b9dda957a51a2c1a7fee00a37455630f5faaae0e
victorlima395 <victorlima395@gmail.com>
2019-10-22 21:51:40 -0300

    Início CRUD

--- ProjetoCRUD/src/univs/edu/usuario/Usuario.java
+++ ProjetoCRUD/src/univs/edu/usuario/Usuario.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package univs.edu.usuario;
 
-/**
- *
- * @author victo
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
+    
+    @Id
+    @GeneratedValue
+    private int idUsuario;
+    
+    @Column(length = 20, nullable = false)
+    private String login;
+    
+    @Column(length = 20, nullable = false)
+    private String senha;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
     
 }
