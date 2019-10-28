09f803c4fe9d731ee728f9cf0f83bf9bc70e2789
victorlima395 <victorlima395@gmail.com>
2019-10-28 20:09:11 -0300

    Início Interface

--- ProjetoCRUD/src/univs/edu/usuario/Usuario.java
+++ ProjetoCRUD/src/univs/edu/usuario/Usuario.java
@@ -5,39 +5,58 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+
 @Entity
 public class Usuario {
     
-    @Id
+    @Id 
     @GeneratedValue
     private int idUsuario;
     
     @Column(length = 20, nullable = false)
-    private String login;
+    private String loguin;
     
     @Column(length = 20, nullable = false)
     private String senha;
 
+    /**
+     * @return the idUsuario
+     */
     public int getIdUsuario() {
         return idUsuario;
     }
 
+    /**
+     * @param idUsuario the idUsuario to set
+     */
     public void setIdUsuario(int idUsuario) {
         this.idUsuario = idUsuario;
     }
 
-    public String getLogin() {
-        return login;
+    /**
+     * @return the loguin
+     */
+    public String getLoguin() {
+        return loguin;
     }
 
-    public void setLogin(String login) {
-        this.login = login;
+    /**
+     * @param loguin the loguin to set
+     */
+    public void setLoguin(String loguin) {
+        this.loguin = loguin;
     }
 
+    /**
+     * @return the senha
+     */
     public String getSenha() {
         return senha;
     }
 
+    /**
+     * @param senha the senha to set
+     */
     public void setSenha(String senha) {
         this.senha = senha;
     }
--- ProjetoCRUD/src/univs/edu/usuario/UsuarioDAO.java
+++ ProjetoCRUD/src/univs/edu/usuario/UsuarioDAO.java
@@ -6,11 +6,10 @@ import org.hibernate.criterion.Restrictions;
 import univs.edu.util.HibernateUtil;
 
 public class UsuarioDAO {
-
     private Session sessao;
     private Transaction transacao;
-
-    public void salvar(Usuario usuario) {
+    
+    public void salvar(Usuario usuario){
         sessao = HibernateUtil.getSessionFactory().openSession();
         transacao = sessao.beginTransaction();
         sessao.save(usuario);
@@ -18,7 +17,7 @@ public class UsuarioDAO {
         sessao.close();
     }
     
-    public void excluir(Usuario usuario) {
+     public void excluir(Usuario usuario){
         sessao = HibernateUtil.getSessionFactory().openSession();
         transacao = sessao.beginTransaction();
         sessao.delete(usuario);
@@ -26,20 +25,21 @@ public class UsuarioDAO {
         sessao.close();
     }
     
-    public void editar(Usuario usuario) {
+     public void editar(Usuario usuario){
         sessao = HibernateUtil.getSessionFactory().openSession();
         transacao = sessao.beginTransaction();
         sessao.update(usuario);
         transacao.commit();
         sessao.close();
     }
-    
-    public Usuario pesquisar(int id) {
+     
+     public Usuario pesquisar(int id){
         sessao = HibernateUtil.getSessionFactory().openSession();
         transacao = sessao.beginTransaction();
         Usuario usuario = (Usuario) sessao.createCriteria(Usuario.class).add(Restrictions.eq("idUsuario", id)).uniqueResult();
         sessao.close();
         return usuario;
     }
-
+     
 }
+
